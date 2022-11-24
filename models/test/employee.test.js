e68536783f2fb91c58c659bc26e6f535bf438742
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-24 12:24:43 +0100

    Add tests for employee.model

---
+++ models/test/employee.test.js
@@ -0,0 +1,42 @@
+const Employee = require("../employee.model.js");
+const expect = require("chai").expect;
+
+describe("Employee", () => {
+  it("should throw an error if there is no arg", () => {
+    const emp = new Employee({}); // create new Department, but don't set `name` attr value
+
+    emp.validate((err) => {
+      expect(err.errors.firstName).to.exist;
+      expect(err.errors.lastName).to.exist;
+      expect(err.errors.department).to.exist;
+    });
+  });
+
+  it("should throw an error if arguments are not a string", () => {
+    const cases = [{}, []];
+    for (let arg of cases) {
+      const emp = new Employee({ arg });
+
+      emp.validate((err) => {
+        expect(err.errors.firstName).to.exist;
+        expect(err.errors.lastName).to.exist;
+        expect(err.errors.department).to.exist;
+      });
+    }
+  });
+
+  it("should not throw an error if args are okay", () => {
+    const cases = [
+      { firstName: "John", lastName: "Doe", department: "IT" },
+      { firstName: "Amanda", lastName: "Watson", department: "IT" },
+    ];
+
+    for (let arg of cases) {
+      const emp = new Employee(arg);
+
+      emp.validate((err) => {
+        expect(err).to.not.exist;
+      });
+    }
+  });
+});
